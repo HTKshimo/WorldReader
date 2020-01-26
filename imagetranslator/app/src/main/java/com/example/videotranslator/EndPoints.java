ec97fc68a37dee630e92e4e921b89b49b349d5a0
Shuang Li <shuangl@iastate.edu>
2020-01-26 04:43:48 -0600

    initial commit. Implemented image upload from storage. Connection to server has not been tested.

---
+++ imagetranslator/app/src/main/java/com/example/videotranslator/EndPoints.java
@@ -0,0 +1,11 @@
+package com.example.videotranslator;
+/**
+ * Created by Belal on 10/24/2017.
+ */
+
+public class EndPoints {
+    //TODO: update url
+//    private static final String ROOT_URL = "http://192.168.101.1/MyApi/Api.php?apicall=";
+    public static final String UPLOAD_URL = "";
+//    public static final String GET_PICS_URL = ROOT_URL + "getpics";
+}
---
+++ imagetranslator/app/src/main/java/com/example/videotranslator/MainActivity.java
@@ -0,0 +1,207 @@
+package com.example.videotranslator;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.error.AuthFailureError;
+import com.android.volley.error.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.net.Authenticator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST = 0;
+    private static final int RESULT_LOAD_IMAGE = 1;
+//    private final int REQUEST_CODE = 1;
+//    private final int VIDEO_REQUEST = 101;
+//    private Uri videoUri = null;
+//
+    ImageView imageView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+        }
+
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+//
+//    public void captureVideo(View v){
+//
+//        Intent i = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+//        if (i.resolveActivity(getPackageManager()) != null){
+//            startActivityForResult(i, VIDEO_REQUEST);
+//        }
+//
+////        File videoFile = getFile();
+////        Uri uri = Uri.fromFile(videoFile);
+////        i.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+////        i.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+////        startActivityForResult(i, REQUEST_CODE);
+//
+//    }
+//
+//    public void playVideo(View view){
+//        Intent playIntent = new Intent(this, VideoPlayActivity.class);
+//        playIntent.putExtra("videoUri", videoUri.toString());
+//        startActivity(playIntent);
+//
+//    }
+//
+//    public File getFile(){
+//        File folder = new File("sdcard/myfolder");
+//        if(!folder.exists()){
+//            folder.mkdir();
+//        }
+//        File videoFile = new File(folder, "myvideo.mp4");
+//        return videoFile;
+//    }
+//
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_REQUEST:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this, "Permission not granted!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case RESULT_LOAD_IMAGE:
+                if (resultCode == RESULT_OK){
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath = cursor.getString(columnIndex);
+                    cursor.close();
+                    Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+                    imageView.setImageBitmap(bitmap);
+                    uploadBitmap(bitmap);
+                }
+        }
+    }
+
+    /*
+     * The method is taking Bitmap as an argument
+     * then it will return the byte[] array for the given bitmap
+     * and we will send this array to the server
+     * here we are using PNG Compression with 80% quality
+     * you can give quality between 0 to 100
+     * 0 means worse quality
+     * 100 means best quality
+     * */
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private void uploadBitmap(final Bitmap bitmap) {
+
+        //getting the tag from the edittext
+//        final String tags = editTextTags.getText().toString().trim();
+
+        //our custom volley request
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, EndPoints.UPLOAD_URL,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        try {
+                            JSONObject obj = new JSONObject(new String(response.data));
+                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            /*
+             * If you want to add more parameters with the image
+             * you can do it here
+             * here we have only one parameter with the image
+             * which is tags
+             * */
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+//                params.put("tags", tags);
+                return params;
+            }
+
+            /*
+             * Here we are passing image by renaming it with a unique name
+             * */
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put("pic", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
+                return params;
+            }
+        };
+
+        //adding the request to volley
+        Volley.newRequestQueue(this).add(volleyMultipartRequest);
+    }
+}
